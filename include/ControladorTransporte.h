85360c821aa80ccab004698e61f5b9dd0b8b0180
Iohana Maria B <iohana.ma31@gmail.com>
2026-07-02 16:17:32 -0300

    refactor: adicionando mais métodos no ControladorDeTransito

--- include/ControladorTransporte.h
+++ include/ControladorTransporte.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <vector>
 #include <string>
+
+// Incluindo todas as entidades que o controlador vai gerenciar
 #include "Cidade.h"
 #include "Trajeto.h"
 #include "Transporte.h"
 #include "Passageiro.h"
 #include "Viagem.h"
+#include "Tipos.h"
 
 class ControladorDeTransito {
 private:
@@ -15,21 +18,28 @@ private:
     std::vector<Passageiro*> passageiros;
     std::vector<Viagem*> viagens;
 
-    // Métodos auxiliares privados são essenciais para manter o código limpo
-    Cidade* buscarCidade(std::string nome);
-    Transporte* buscarTransporte(std::string nome);
-    Passageiro* buscarPassageiro(std::string nome);
+    Cidade* buscarCidade(const std::string& nome);
+    Transporte* buscarTransporte(const std::string& nome);
+    Passageiro* buscarPassageiro(const std::string& nome);
 
 public:
+    ControladorDeTransito();
     ~ControladorDeTransito();
-    
+
+    // Métodos de Cadastro
     void cadastrarCidade(std::string nome);
     void cadastrarTrajeto(std::string nomeOrigem, std::string nomeDestino, TipoTerreno tipo, int distancia);
-    void cadastrarTransporte(std::string nome, TipoTerreno tipo, int capacidade, int velocidade, int distDescanso, int tempoDescanso, std::string nomeCidade);
-    void cadastrarPassageiro(std::string nome, std::string nomeCidade);
+    void cadastrarTransporte(std::string nome, TipoTerreno tipo, int capacidade, int velocidade, int distancia_entre_descansos, int tempo_de_descanso, std::string localAtual);
+    void cadastrarPassageiro(std::string nome, std::string localAtual);
     
+    // Gestão de Viagens e Simulação
     void iniciarViagem(std::string nomeTransporte, std::vector<std::string> nomesPassageiros, std::string nomeOrigem, std::string nomeDestino);
     void avancarHoras(int horas);
     
+    // Relatórios solicitados no escopo do projeto
     void relatarEstado();
+    void relatarOndeEstaoPessoas();
+    void relatarOndeEstaoTransportes();
+    void relatarViagensEmAndamento();
+    void relatarCidadesMaisVisitadas();
 };
